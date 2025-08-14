8288f7db2e9fcd2cbabfe182adebcceef0a64690
Idarraga14 <164957610+Idarraga14@users.noreply.github.com>
2025-08-14 15:03:28 -0500

    Creacion proyecto base

---
+++ co.edu.uniquindio.clientefx/src/main/java/co/edu/uniquindio/cliente/clienteapp/Launcher.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.cliente.clienteapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ClienteApplication.class, args);
+    }
+}
